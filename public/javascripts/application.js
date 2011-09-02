6fd5b6399fb766c0be104f71ff06812ac833c310
Louis Liu <lliu@hgsamerica.com>
2011-09-01 23:06:01 -0400

    fix shopping cart.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,14 +4,19 @@ $('div.page').live('pagebeforecreate',function(event){
   $('header', this).attr('data-role', "header").attr('data-theme', "b");
   $('footer', this).attr('data-role', "footer").attr('data-theme', "b");
   $('div.content', this).attr('data-role', "content");
+  $('div.collapsible-set', this).attr('data-role', "collapsible-set");
+  $('div.collapsible', this).attr('data-role', "collapsible");
+  $('div.collapsibled', this).attr('data-collapsible', "true");
   $('a.home', this).attr('data-icon', "home").attr('data-direction', "reverse");
   $('a.back', this).attr('data-icon', "back").attr('data-direction', "reverse");
   $('a.info', this).attr('data-icon', "info");
   $('a.alert', this).attr('data-icon', "alert");
   $('a.inline', this).attr('data-role', "button").attr('data-inline', "true");
-  $('ul.listview', this).attr('data-role', "listview").attr('data-inset', "true");
+  $('ul.listview', this).attr('data-role', "listview")
+  $('ul.inset', this).attr('data-inset', "true");
   $('ul.listview', this).has('li').attr('data-filter',"true").attr('data-filter-placeholder',"Filter...");
-  $('ul.listview a:nth-child(2)', this).attr('data-icon', "plus");
+  $('ul.listview a:nth-child(2).plus', this).attr('data-icon', "plus");
+  $('ul.listview a:nth-child(2).minus', this).attr('data-icon', "minus");
   $('div.field', this).attr('data-role', 'fieldcontain');
   $('form', this).has(':file').attr('data-ajax', "false");
   $('a.b', this).attr('data-theme', 'b');
