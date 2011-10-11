7df46b73ec0d68c34b7d9c9dfc60bcd691ef68d8
Louis Liu <lliu@hgsamerica.com>
2011-10-11 17:19:45 -0400

    upgrade jquerymobile to 1.0rc1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,15 @@ $('div.page').live('pagebeforecreate',function(event){
   $('footer', this).attr('data-role', "footer").attr('data-theme', "b");
   $('div.content', this).attr('data-role', "content");
   $('div.collapsible-set', this).attr('data-role', "collapsible-set");
-  $('div.collapsible', this).attr('data-role', "collapsible");
-  $('div.collapsibled', this).attr('data-collapsible', "true");
+  $('div.collapsible', this).attr('data-role', "collapsible").attr('data-content-theme', "b");
+  $('div.collapsibled', this).attr('data-collapsed', "true");
+  $('div.expanded', this).attr('data-collapsed', "false");
   $('a.home', this).attr('data-icon', "home").attr('data-direction', "reverse");
   $('a.back', this).attr('data-icon', "back").attr('data-direction', "reverse");
   $('a.info', this).attr('data-icon', "info");
   $('a.alert', this).attr('data-icon', "alert");
   $('a.inline', this).attr('data-role', "button").attr('data-inline', "true");
-  $('ul.listview', this).attr('data-role', "listview")
+  $('ul.listview', this).attr('data-role', "listview");
   $('ul.inset', this).attr('data-inset', "true");
   $('ul.listview', this).has('li').attr('data-filter',"true").attr('data-filter-placeholder',"Filter...");
   $('ul.listview a:nth-child(2).plus', this).attr('data-icon', "plus");
